72c087573d2bbd56efc8e1f7c7a2dc1cd3e00abe
Victor <vhfurtuoso@gmail.com>
2025-05-01 14:49:56 -0300

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,12 @@
 import express from "express";
 import logger from "morgan";
 import * as path from "path";
-
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
-
-// Routes
 import { router } from "./routes/index";
 import { json } from 'body-parser';
-// Create Express server
+
 export const app = express();
 
-// Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
